409e25efa0806df28b8d7151171defd42490fb47
Johannes Haseitl <johannes@undpaul.de>
2014-11-20 10:39:10 +0100

    Added new entity type: event

---
+++ modules/custom/example/src/Entity/Event.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\example\Entity;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\Annotation\FieldType;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+
+/**
+ * @ContentEntityType (
+ *   id = "event",
+ *   label = @Translation("Event"),
+ *   handlers = {
+ *   },
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "label" = "title",
+ *   }
+ * )
+ */
+class Event extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $definitions = array();
+
+    $definitions['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $definitions['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setReadOnly(TRUE);
+
+    $definitions['title'] = BaseFieldDefinition::create('string')
+      ->setLabel('Title')
+      ->setRequired(TRUE)
+      ->setSettings('max_length', 255);
+
+    return $definitions;
+  }
+
+
+}
